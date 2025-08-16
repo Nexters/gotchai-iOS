ba315104805e7a8251e70e4db7ec89a80494e3d3
koreamango <mango1229@icloud.com>
2025-08-17 04:42:52 +0900

    feat: di in App

--- Gotchai/App/Sources/GotchaiApp.swift
+++ Gotchai/App/Sources/GotchaiApp.swift
@@ -1,17 +1,32 @@
 import TCA
 import SwiftUI
+import Key
+import Auth
+import CustomNetwork
+import Common
+import Moya
 
 @main
 struct GotchaiApp: App {
+
+    private let store: StoreOf<AppFeature>
+
+    init() {
+        let tokenStore: TokenProvider = KeychainTokenProvider.shared
+        let provider = MoyaProvider<MultiTarget>(plugins: [AuthPlugin(tokenStore: tokenStore)])
+        let refresher: TokenRefresherProtocol = TokenRefresher(provider: provider, tokenStore: tokenStore)
+        let networkClient = MoyaAPIClient(provider: provider, refresher: refresher)
+
+        self.store = Store(initialState: AppFeature.State()) {
+            AppFeature()
+        } withDependencies: {
+            $0.networkClient = networkClient   // ✅ 여기서 한 번만 주입
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            AppView(
-                store: StoreOf<AppFeature>(initialState: AppFeature.State(), reducer: {
-                    AppFeature()
-                }, withDependencies: { dep in
-
-                })
-            )
+            AppView(store: store)
         }
     }
 }
