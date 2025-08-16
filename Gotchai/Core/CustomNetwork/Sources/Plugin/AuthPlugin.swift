0feabfe7c969efc1c161acd40df568ce09c2fd71
koreamango <mango1229@icloud.com>
2025-08-17 02:33:36 +0900

    chore: Auth Plugin 코드 정리

--- Gotchai/Core/CustomNetwork/Sources/Plugin/AuthPlugin.swift
+++ Gotchai/Core/CustomNetwork/Sources/Plugin/AuthPlugin.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 import Moya
-import Security
 import Key
 
-/// 요청 전 Token을 넣어주는 Plugin
 public final class AuthPlugin: PluginType {
     public init() {}
+
+    /// 요청 전 Token을 넣어주는 메소드
     public func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
         guard let token = KeychainTokenProvider.shared.accessToken else { return request }
         var req = request
