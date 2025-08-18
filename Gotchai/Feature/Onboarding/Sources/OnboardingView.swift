2112bc651dd37cd9abb0be7f7623c357b0162995
slr-09 <ge12090@naver.com>
2025-08-19 02:10:43 +0900

    UI: 디자인 QA 온보딩 뷰

--- Gotchai/Feature/Onboarding/Sources/OnboardingView.swift
+++ Gotchai/Feature/Onboarding/Sources/OnboardingView.swift
@@ -5,105 +5,92 @@
 //  Created by koreamango on 7/28/25.
 //
 
+import DesignSystem
 import SwiftUI
 import TCA
-import DesignSystem
 
 public struct OnboardingView: View {
-  @Bindable var store: StoreOf<OnboardingFeature> 
+    @Bindable var store: StoreOf<OnboardingFeature>
 
-  public init(store: StoreOf<OnboardingFeature>) {
-    self.store = store
-  }
-
-  public var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { viewStore in
-      ZStack {
-          Color(.gray_950)
-              .ignoresSafeArea()
-
-        VStack {
-          HStack {
-            Spacer()
-              if (viewStore.currentPage < viewStore.pages.count - 1) {
-                  Button("건너뛰기") {
-                    viewStore.send(.start)
-                  }
-                  .foregroundColor(Color(.gray_400))
-                  .padding()
-              }
-          }
-
-          Spacer()
-
-          TabView(selection: viewStore.binding(
-            get: \.currentPage,
-            send: OnboardingFeature.Action.pageChanged
-          )) {
-            ForEach(Array(viewStore.pages.enumerated()), id: \.offset) {
- idx,
- page in
-              VStack(spacing: 24) {
-                  Image(page.imageName, bundle: .module)
-                  .resizable()
-                  .scaledToFit()
-                  .frame(width: 300, height: 300)
-                  .cornerRadius(16)
-
-                Text(page.title)
-                  .foregroundColor(Color(.gray_white))
-                  .multilineTextAlignment(.center)
-                  .padding(.horizontal, 32)
-              }
-              .tag(idx)
-            }
-          }
-          .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-          .frame(height: 400)
-
-          HStack(spacing: 8) {
-            ForEach(0..<viewStore.pages.count, id: \.self) { idx in
-              Circle()
-                .fill(
-                  idx == viewStore.currentPage ?
-                  Color(.gray_400) :
-                    Color(.gray_700)
-                )
-                .frame(width: 8, height: 8)
-            }
-          }
-          .padding(.vertical, 24)
-
-          Spacer()
-
-          Button {
-            viewStore.send(.nextButtonTapped)
-          } label: {
-            Text(viewStore.currentPage < viewStore.pages.count - 1 ? "다음" : "시작하기")
-              .bold()
-              .frame(maxWidth: .infinity)
-              .padding()
-              .background(Color(.gray_400))
-              .cornerRadius(12)
-              .foregroundColor(.black)
-          }
-          .padding(.horizontal, 24)
-          .padding(.bottom, 40)
-        }
-      }
-      .navigationBarBackButtonHidden()
+    public init(store: StoreOf<OnboardingFeature>) {
+        self.store = store
     }
-  }
+
+    public var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            ZStack {
+                Color(.gray_950).ignoresSafeArea()
+
+                VStack {
+                    HStack {
+                        Spacer()
+                        if viewStore.currentPage < viewStore.pages.count - 1 {
+                            Button("건너뛰기") {
+                                viewStore.send(.start)
+                            }
+                            .fontStyle(.body_5)
+                            .foregroundColor(Color(.gray_400))
+                            .padding(.horizontal, 32)
+                            .padding(.top, 10)
+                        }
+                    }
+                    .frame(height: 22)
+
+                    TabView(selection: viewStore.binding(
+                        get: \.currentPage,
+                        send: OnboardingFeature.Action.pageChanged
+                    )) {
+                        ForEach(Array(viewStore.pages.enumerated()), id: \.offset) {
+                            idx,
+                                page in
+                            VStack(spacing: 44) {
+                                Image(page.imageName, bundle: .module)
+
+                                Text(page.title)
+                                    .fontStyle(.body_1)
+                                    .foregroundColor(Color(.gray_white))
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal, 8)
+                            }
+                            .tag(idx)
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+                    HStack(spacing: 8) {
+                        ForEach(0 ..< viewStore.pages.count, id: \.self) { idx in
+                            Circle()
+                                .fill(
+                                    idx == viewStore.currentPage ?
+                                        Color(.primary_400) :
+                                        Color(.gray_700)
+                                )
+                                .frame(width: 6, height: 6)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                    .padding(.bottom, 32)
+
+                    CTAButton(text: viewStore.currentPage < viewStore.pages.count - 1 ? "다음" : "시작하기") {
+                        viewStore.send(.nextButtonTapped)
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 10)
+                }
+            }
+            .navigationBarBackButtonHidden()
+        }
+    }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-  static var previews: some View {
-    OnboardingView(
-      store: StoreOf<OnboardingFeature>(
-        initialState: OnboardingFeature.State()
-      ) {
-        OnboardingFeature()
-      }
-    )
-  }
+    static var previews: some View {
+        OnboardingView(
+            store: StoreOf<OnboardingFeature>(
+                initialState: OnboardingFeature.State()
+            ) {
+                OnboardingFeature()
+            }
+        )
+    }
 }
