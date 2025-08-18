860a796a028863b4a30ce3a5e035ad6bc7640407
koreamango <mango1229@icloud.com>
2025-08-19 03:14:47 +0900

    fix: 뱃지 카드 롤백

--- Gotchai/Feature/Main/Sources/TuringTest/BadgeCardView.swift
+++ Gotchai/Feature/Main/Sources/TuringTest/BadgeCardView.swift
@@ -18,38 +18,33 @@ struct BadgeCardView: View {
     var body: some View {
         let badgeCardColor = GradientHelper.getBadgeColors(for: badge.tier)
 
-        return ZStack {
-            Color(.gray_950)
-                .ignoresSafeArea()
-            VStack(spacing: 0) {
-                if let uiImage = badge.image {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .frame(width: 213, height: 213)
-                        .clipShape(Circle())
-                }
-
-                Text(
-                    badge.correctCount == 7 ? "모두 맞춘 당신은" : "7개 중 \(badge.correctCount)개를 맞춘 당신은"
-                )
-                .fontStyle(.body_1)
-                .foregroundStyle(Color(hex: badgeCardColor.subColor))
-                .padding(.top, 26)
-                Text(badge.badgeName)
-                    .fontStyle(.title_3)
-                    .foregroundStyle(Color(hex: badgeCardColor.titleColor))
-                Text(badge.description)
-                    .fontStyle(.body_4)
-                    .foregroundStyle(Color(.gray_white).opacity(0.7))
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 16)
-                Image(badgeCardColor.image, bundle: .module)
-                    .padding(.top, 36)
+        return VStack(spacing: 0) {
+            if let uiImage = badge.image {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 213, height: 213)
+                    .clipShape(Circle())
             }
-            .padding([.horizontal, .top], 34)
+
+            Text(
+                badge.correctCount == 7 ? "모두 맞춘 당신은" : "7개 중 \(badge.correctCount)개를 맞춘 당신은"
+            )
+            .fontStyle(.body_1)
+            .foregroundStyle(Color(hex: badgeCardColor.subColor))
+            .padding(.top, 26)
+            Text(badge.badgeName)
+                .fontStyle(.title_3)
+                .foregroundStyle(Color(hex: badgeCardColor.titleColor))
+            Text(badge.description)
+                .fontStyle(.body_4)
+                .foregroundStyle(Color(.gray_white).opacity(0.7))
+                .multilineTextAlignment(.center)
+                .padding(.top, 16)
+            Image(badgeCardColor.image, bundle: .module)
+                .padding(.top, 36)
+        }.padding([.horizontal, .top], 34)
             .padding(.bottom, 27)
-            .frame(maxWidth: .infinity, minHeight: 400)
-            .aspectRatio(badgeAspect, contentMode: .fit)
+            .frame(maxWidth: .infinity, minHeight: 481)
             .gradientBackground(
                 stops: badgeLinearBackground,
                 startPoint: .topLeading,
@@ -66,8 +61,6 @@ struct BadgeCardView: View {
                     endRadius: 400
                 ).opacity(0.2)
             )
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 24))
+            .clipShape(RoundedRectangle(cornerRadius: 24))
     }
 }
-
